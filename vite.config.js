a716decfff5651f465d33114a1778c3a824fbdeb
Flory-code <drallini57@gmail.com>
2025-05-16 12:35:06 +0300

    pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(),tailwindcss()],
-  
+    base: "/REPONAME/",
+    plugins: [vue(),tailwindcss()],
+    resolve: {
+    alias: [
+      
+    ]
+  },
 })
